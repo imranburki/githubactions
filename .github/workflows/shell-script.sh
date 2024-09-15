e44ffd4c89f10e31be051f0d77fd43fc22daa2d7
Imran Burki <imranburki438@gmail.com>
2024-09-16 00:11:54 +0500

    changing file name to dragon.txt

--- .github/workflows/shell-script.sh
+++ .github/workflows/shell-script.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 sudo apt-get install cowsay -y
 cowsay 'Hello, Linux!'
-cowsay -f dragon "Run for covre, I AM DRAGON" >> cowsays.txt
+cowsay -f dragon "Run for covre, I AM DRAGON" >> dragon.txt
 grep -i "dragon" dragon.txt  
 cat dragon.txt
 ls -ltra
