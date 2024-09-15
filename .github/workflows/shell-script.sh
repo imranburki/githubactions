e946f6cbf540b8c7bf49e566c5ff72376ea694a6
Imran Burki <imranburki438@gmail.com>
2024-09-16 00:08:35 +0500

    fixing error in shell-script.sh

--- .github/workflows/shell-script.sh
+++ .github/workflows/shell-script.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-sudo apt-get install cowsays -y
+sudo apt-get install cowsay -y
 cowsay -f dragon "Run for covre, I AM DRAGON" >> cowsays.txt
 grep -i "dragon" dragon.txt  
 cat dragon.txt
